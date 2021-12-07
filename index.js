b609746677b2c45bf453e2a3629e866bb28d3e72
vinisp <aragao93.vinicius@gmail.com>
2021-12-07 17:26:53 -0300

    api

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.info("heloo mundo");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Word");
+});
+
+app.listen(PORT, () => {
+  console.info(`${PORT}`);
+});
